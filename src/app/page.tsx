c4f63446ab76eccb6a6bc8eb0a1d37ab30a28829
Pankaj Vatwani <vatwanipankaj74@gmail.com>
2024-06-18 22:05:19 +0530

    Feedback Changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import Image from "next/image";
 
+import { Progress } from "@/components/ui/progress";
 import CopyIcon from "@/components/icons/copy-icon";
 import CodeIcon from "@/components/icons/code-icon";
 import { Checkbox } from "@/components/ui/checkbox";
@@ -17,10 +18,45 @@ export default function Home() {
 	const [usePremium, setUsePremium] = React.useState(false);
 	const [uploadedImageId, setUploadedImageId] = React.useState<string | null>(null);
 
-	const [loading, setLoading] = React.useState(false);
+	const [isButtonsDisabled, setIsButtonsDisabled] = React.useState(false);
+
+	const [showSpinner, setShowSpinner] = React.useState(false);
+
+	const [progress, setProgress] = React.useState(0);
+	const [useProgress, setUseProgress] = React.useState(false);
+	const [showProgress, setShowProgress] = React.useState(false);
+
+	React.useEffect(() => {
+		if (!useProgress) return;
+
+		setShowProgress(true);
+		const timeouts: any[] = [];
+
+		const updateProgress = (newProgress: number, delay: number) => {
+			const timeout = setTimeout(() => setProgress(newProgress), delay);
+			timeouts.push(timeout);
+		};
+
+		updateProgress(20, 500);
+		updateProgress(45, 1000);
+		updateProgress(70, 1800);
+		updateProgress(85, 2800);
+		updateProgress(100, 3500);
+
+		const hideProgressTimeout = setTimeout(() => {
+			setProgress(100); // Ensure progress is 100% before hiding
+			setShowProgress(false);
+			setUseProgress(false);
+		}, 4000);
+
+		timeouts.push(hideProgressTimeout);
+
+		return () => timeouts.forEach(clearTimeout); // Clear timeouts on component unmount
+	}, [useProgress]);
 
 	const optimizeImage = async () => {
-		setLoading(true);
+		setShowSpinner(true);
+		setIsButtonsDisabled(true);
 
 		if (file === null) {
 			toast({ title: "No file selected", description: "Please select a file to optimize", variant: "destructive" });
@@ -43,11 +79,17 @@ export default function Home() {
 			}
 		}
 
-		setLoading(false);
+		setShowSpinner(false);
+		setUseProgress(true);
+
+		setTimeout(() => {
+			setIsButtonsDisabled(false);
+		}, 4350);
 	};
 
 	const downloadImage = async () => {
-		setLoading(true);
+		setIsButtonsDisabled(true);
+		setShowSpinner(true);
 
 		if (uploadedImageId === null) {
 			toast({ title: "No image to download", description: "Please submit an image optimization request first", variant: "destructive" });
@@ -68,13 +110,24 @@ export default function Home() {
 			}
 		}
 
-		setLoading(false);
+		setShowSpinner(false);
+		setIsButtonsDisabled(false);
 	};
 
 	return (
 		<>
 			<div className="flex justify-center items-center h-screen">
 				<div className="w-full mx-10 md:mx-20">
+
+					{showProgress && (
+						<div className="mb-8">
+							<Progress
+								max={100}
+								value={progress}
+							/>
+						</div>
+					)}
+
 					<FileUploader
 						containerClassName="w-full text-center"
 						accept={{
@@ -86,29 +139,37 @@ export default function Home() {
 						}}
 						multiple={undefined} onDragEnter={undefined} onDragOver={undefined} onDragLeave={undefined}
 					>
-						<div className="rounded-full border border-grey-50 bg-white p-2">{!file ? <DocumentUploadIcon className="text-7xl" /> : <Image src={URL.createObjectURL(file)} alt="logo" width={100} height={100} />}</div>
-						<p className="mt-4 text-base font-bold text-gray-500">
-							{file ? file.name.slice(0, 16) : "Drop your files here or"} <span className="cursor-pointer hover:underline">{file ? "selected" : "browse explorer"}</span>
-						</p>
+						{showSpinner ? (
+							<div className="flex items-center justify-center">
+								<div className="animate-spin rounded-full h-28 w-28 border-t-4 border-b-4 border-gray-800"></div>
+							</div>
+						) : (
+							<>
+								<div className="rounded-full border border-grey-50 bg-white p-2">{!file ? <DocumentUploadIcon className="text-7xl" /> : <Image src={URL.createObjectURL(file)} alt="logo" width={100} height={100} />}</div>
+								<p className="mt-4 text-base font-bold text-gray-500">
+									{file ? file.name.slice(0, 16) : "Drop your files here or"} <span className="cursor-pointer hover:underline">{file ? "selected" : "browse explorer"}</span>
+								</p>
+							</>
+						)}
 					</FileUploader>
 
 					<div className="flex flex-col md:flex-row items-center justify-content-center mt-6 md:space-x-5 space-y-3 md:space-y-0">
-						<button disabled={loading} onClick={optimizeImage} className="bg-gray-800 disabled:bg-gray-300 text-white text-lg font-bold px-6 py-3 tracking-wider rounded-sm w-full">
+						<button disabled={isButtonsDisabled} onClick={optimizeImage} className="bg-gray-800 disabled:bg-gray-300 text-white text-lg font-bold px-6 py-3 tracking-wider rounded-sm w-full">
 							<CodeIcon className="text-3xl inline-block mr-2" />
 							OPTIMIZE
 						</button>
 
-						<button disabled={loading} onClick={downloadImage} className="bg-gray-800 disabled:bg-gray-300 text-white text-lg font-bold px-6 py-3 tracking-wider rounded-sm w-full">
+						<button disabled={isButtonsDisabled} onClick={downloadImage} className="bg-gray-800 disabled:bg-gray-300 text-white text-lg font-bold px-6 py-3 tracking-wider rounded-sm w-full">
 							<CopyIcon className="text-3xl inline-block mr-2" />
 							DOWNLOAD
 						</button>
 					</div>
 
 					<div className="items-top flex space-x-2 mt-8">
-						<Checkbox 
+						<Checkbox
 							checked={usePremium}
 							onCheckedChange={(checked: any) => setUsePremium(checked)}
-							id="use_premium" 
+							id="use_premium"
 						/>
 						<div className="grid gap-1.5 leading-none">
 							<label
